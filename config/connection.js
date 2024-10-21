a5e058b84499559ca4932ccc56b29dbd638d5384
Tyler Barrios <tylerbarrios@gmail.com>
2024-10-21 14:24:33 -0400

    Update connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,27 +1,18 @@
 require("dotenv").config();
-
 const { Sequelize } = require("sequelize");
 
-const sequelize = process.env.DATABASE_URL
-  ? new Sequelize(process.env.DATABASE_URL, {
-      dialect: "postgres",
-      protocol: "postgres",
-      dialectOptions: {
-        ssl: {
-          require: true,
-          rejectUnauthorized: false
-        }
-      }
-    })
-  : new Sequelize(
-      process.env.DB_NAME,
-      process.env.DB_USER,
-      process.env.DB_PASSWORD,
-      {
-        host: process.env.DB_HOST || "localhost",
-        dialect: "postgres",
-        port: process.env.DB_PORT || 5432
-      }
-    );
+const sequelize = new Sequelize(process.env.DATABASE_URL || "", {
+  dialect: "postgres",
+  protocol: "postgres",
+  dialectOptions: {
+    ssl: process.env.DATABASE_URL ? {
+      require: true,
+      rejectUnauthorized: false, 
+    } : false, 
+  },
+  host: process.env.DB_HOST || "localhost",
+  port: process.env.DB_PORT || 5432,
+  logging: false, 
+});
 
 module.exports = sequelize;
